001a01f195910a094afc4bf3edd6e81248107af9
baichangda <471267877@qq.com>
2021-12-10 14:52:20 +0800

    c

--- src/main/java/com/bcd/ickey/Parser_ICKey.java
+++ src/main/java/com/bcd/ickey/Parser_ICKey.java
@@ -2,11 +2,15 @@ package com.bcd.ickey;
 
 import com.bcd.base.exception.BaseRuntimeException;
 import com.bcd.base.util.DateZoneUtil;
+import com.bcd.ickey.data.PacketData;
 import com.bcd.parser.Parser;
 import com.bcd.parser.processer.FieldDeProcessContext;
 import com.bcd.parser.processer.FieldProcessContext;
 import com.bcd.parser.processer.FieldProcessor;
+import com.bcd.parser.util.PerformanceUtil;
 import io.netty.buffer.ByteBuf;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -16,6 +20,7 @@ import java.util.Objects;
 
 @Component
 public class Parser_ICKey extends Parser {
+
     @Override
     protected List<Class> getParsableClass() {
         return getParsableClassByScanPackage("com.bcd.ickey");
@@ -25,6 +30,20 @@ public class Parser_ICKey extends Parser {
         super(false);
         setDateProcessor(new DateFieldProcessor());
         init();
+    }
+
+    public static void main(String[] args) {
+        Parser parser= new Parser_ICKey();
+        parser.init();
+
+        String data="292932005B0A594B31323031343135370000023900000000FFFFFFFF010AAAA900004155141407060A0D27073E339002527EA4010A00320001000000010D6A0C00000A0B00030200092E3D011301090109010902D503E800910000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000055AA806B899F0D";
+        int threadNum=1;
+        if(args.length>=1){
+            threadNum=Integer.parseInt(args[0]);
+        }
+        parser.logger.info("param threadNum[{}]",threadNum);
+        int num=100000000;
+        PerformanceUtil.testMultiThreadPerformance(data,parser, PacketData.class,threadNum,num,true);
     }
 }
 
